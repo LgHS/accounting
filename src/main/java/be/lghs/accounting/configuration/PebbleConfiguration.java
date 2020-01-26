0018cf83ac460f5d24430577166958b2ba4372c4
bendem <online@bendem.be>
2020-01-26 18:12:01 +0100

    Fix movement table overflowing its container

--- src/main/java/be/lghs/accounting/configuration/PebbleConfiguration.java
+++ src/main/java/be/lghs/accounting/configuration/PebbleConfiguration.java
@@ -1,8 +1,10 @@
 package be.lghs.accounting.configuration;
 
 import be.lghs.accounting.services.UserService;
+import com.mitchellbosecke.pebble.error.PebbleException;
 import com.mitchellbosecke.pebble.extension.AbstractExtension;
 import com.mitchellbosecke.pebble.extension.Extension;
+import com.mitchellbosecke.pebble.extension.Filter;
 import com.mitchellbosecke.pebble.extension.Function;
 import com.mitchellbosecke.pebble.template.EvaluationContext;
 import com.mitchellbosecke.pebble.template.PebbleTemplate;
@@ -40,14 +42,36 @@ public class PebbleConfiguration {
                     "csrf_param", function((args, self) -> getCsrf().getParameterName()),
 
                     "has_admin_role", function((args, self) -> userService.getCurrentUser()
-                                .map(OAuth2User::getAuthorities)
-                                .map(authorities -> authorities.contains(Roles.ADMIN_AUTHORITY))
-                                .orElse(false)),
+                        .map(OAuth2User::getAuthorities)
+                        .map(authorities -> authorities.contains(Roles.ADMIN_AUTHORITY))
+                        .orElse(false)),
 
                     "has_treasurer_role", function((args, self) -> userService.getCurrentUser()
-                                .map(OAuth2User::getAuthorities)
-                                .map(authorities -> authorities.contains(Roles.TREASURER_AUTHORITY))
-                                .orElse(false))
+                        .map(OAuth2User::getAuthorities)
+                        .map(authorities -> authorities.contains(Roles.TREASURER_AUTHORITY))
+                        .orElse(false))
+                );
+            }
+
+            @Override
+            public Map<String, Filter> getFilters() {
+                return Map.of(
+                    "ellipsis", new Filter() {
+                        @Override
+                        public Object apply(Object input, Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) throws PebbleException {
+                            String value = input.toString();
+                            if (value.length() <= 7) {
+                                return value;
+                            }
+
+                            return value.substring(0, 3) + "…" + value.substring(value.length() - 3);
+                        }
+
+                        @Override
+                        public List<String> getArgumentNames() {
+                            return List.of();
+                        }
+                    }
                 );
             }
         };
