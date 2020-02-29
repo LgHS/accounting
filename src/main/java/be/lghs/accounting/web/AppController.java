789845521217d327f421ea0680745015f0cc07a9
bendem <online@bendem.be>
2020-02-29 17:37:04 +0100

    Make dashboard graphs cacheable

--- src/main/java/be/lghs/accounting/web/AppController.java
+++ src/main/java/be/lghs/accounting/web/AppController.java
@@ -1,11 +1,11 @@
 package be.lghs.accounting.web;
 
-import be.lghs.accounting.configuration.Roles;
 import be.lghs.accounting.repositories.AccountRepository;
 import be.lghs.accounting.repositories.MovementRepository;
 import be.lghs.accounting.services.GraphService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +18,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 @RequestMapping("/")
@@ -55,6 +56,11 @@ public class AppController {
     @GetMapping(value = "/graphs/rolling-sum", produces = "image/svg+xml")
     public void rollingSum(HttpServletResponse response) throws IOException {
         response.setContentType("image/svg+xml");
+        response.setHeader(HttpHeaders.PRAGMA, "");
+        response.setHeader(HttpHeaders.CACHE_CONTROL, CacheControl
+            .maxAge(4, TimeUnit.HOURS)
+            .cachePublic()
+            .getHeaderValue());
         try (ServletOutputStream output = response.getOutputStream()) {
             graphService.generateRollingSumGraph(output);
         }
@@ -63,6 +69,11 @@ public class AppController {
     @GetMapping(value = "/graphs/credits-per-day", produces = "image/svg+xml")
     public void creditsPerDay(HttpServletResponse response) throws IOException {
         response.setContentType("image/svg+xml");
+        response.setHeader(HttpHeaders.PRAGMA, "");
+        response.setHeader(HttpHeaders.CACHE_CONTROL, CacheControl
+            .maxAge(4, TimeUnit.HOURS)
+            .cachePublic()
+            .getHeaderValue());
         try (ServletOutputStream output = response.getOutputStream()) {
             graphService.generateCreditsPerDayGraph(output);
         }
