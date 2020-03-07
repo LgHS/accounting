f43f2e043fcbb671b00e5497fee5fd3960efabfa
bendem <online@bendem.be>
2020-03-07 16:11:11 +0100

    Try and make jooq happy

---
+++ etc/database.sql
@@ -0,0 +1,14 @@
+create user lghs_accounting_root with password 'lghs_accounting_root_password'; -- change it really
+create database lghs_accounting owner lghs_accounting_root;
+
+\c lghs_accounting
+revoke all on schema public from public;
+
+create user lghs_accounting_app;
+grant connect on database lghs_accounting to lghs_accounting_app;
+
+grant usage on schema public to lghs_accounting_root;
+grant usage on schema public to lghs_accounting_app;
+
+create extension "uuid-ossp";
+create extension pgcrypto;
